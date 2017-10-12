c1338d19c11cc55a7a03c0d701f8391ad539f56a
Helen Pitt <helenpitt@Helens-MacBook-Pro.local>
2017-10-11 22:03:32 -0400

    htmlimgbootstrap

---
+++ Assets/javascript/gemgamejava.js
@@ -0,0 +1,15 @@
+//Ready Function set wins & losses to zero.
+$(document).ready(function(){
+    
+    crystals = ['assets/images/red.png','assets/images/green.png','assets/images/blue.png','assets/images/yellow.png']
+
+
+
+})
+//RandomNumber is generated 19-120
+//Four gems that have random numbers assigned to them 1-12
+//click the gem to generate points based on the assigned numbers
+//numbers add up at the bottom
+//If you get the exact number you win
+//win will be tallied
+//If you can't add up to the exact number you lose.
