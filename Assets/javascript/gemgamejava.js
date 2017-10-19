8a0a2f35c95b5070635e4e3c1c80702c58195e9b
Helen Pitt <helenpitt@Helens-MacBook-Pro.local>
2017-10-18 20:54:07 -0400

    latest version

--- Assets/javascript/gemgamejava.js
+++ Assets/javascript/gemgamejava.js
@@ -12,10 +12,11 @@ $(document).ready(function () {
     newGame();
 
     function newCrystals() {
-        var numbers = gems
+        var numbers = []
 
         while (numbers.length < 4) {
-            var RandomNumber = Math.ceil(Math.random() * 12)
+            var RandomNumber = Math.ceil(Math.random() * 12) 
+            numbers.push(RandomNumber)
         }
         console.log(numbers);
 
@@ -26,26 +27,22 @@ $(document).ready(function () {
             var redGem = $('#button1');
             redGem.attr('data-num', numbers[i]);
             redGem.attr('alt', 'crystals');
-            redGem.addClass('crystalImage')
-            $('#crystals').append(redGem);
+            $('#button1').append(redGem);
 
             var blueGem = $('#button2');
             blueGem.attr('data-num', numbers[i]);
             blueGem.attr('alt', 'crystals');
-            blueGem.addClass('crystalImage')
-            $('#crystals').append(blueGem);
+            $('#button2').append(blueGem);
 
             var greenGem = $('#button3');
             greenGem.attr('data-num', numbers[i]);
             greenGem.attr('alt', 'crystals');
-            greenGem.addClass('crystalImage')
-            $('#crystals').append(greenGem);
+            $('#button3').append(greenGem);
 
             var yellowGem = $('#button4');
             yellowGem.attr('data-num', numbers[i]);
             yellowGem.attr('alt', 'crystals');
-            yellowGem.addClass('crystalImage')
-            $('#crystals').append(yellowGem);
+            $('#button4').append(yellowGem);
 
         }
     }
@@ -69,18 +66,23 @@ $(document).ready(function () {
 
         $('#button1').on('click', function () {
             counter = counter + parseInt($(this).data('num'));
+            $('#yourScore').text(counter)
+            console.log (counter);
         })
 
         $('#button2').on('click', function () {
             counter = counter + parseInt($(this).data('num'));
+            $('#yourScore').text(counter)
         })
 
         $('#button3').on('click', function () {
             counter = counter + parseInt($(this).data('num'));
+            $('#yourScore').text(counter)
         })
 
         $('#button4').on('click', function () {
             counter = counter + parseInt($(this).data('num'));
+            $('#yourScore').text(counter)
         })
 
         $('#yourScore').text(counter)
